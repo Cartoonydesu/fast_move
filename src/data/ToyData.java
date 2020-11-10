ce982ef9713d7d7e3787317c7d39379b09bcf53b
Piraya Sutthiparinyanon <toon.pi45@gmail.com>
2020-11-10 16:57:27 +0700

    cartoon ver

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -3,26 +3,48 @@ package data;
 import base.Bike;
 import base.Parcel;
 import base.Person;
+import base.Location;
+import base.ParcelStatus;
 
 public class ToyData {
 
     public static Bike[] getBikes() {
         Bike[] bikes = new Bike[3];
-        // your code
-
+        Bike b1 = new Bike("b1",1);
+        Bike b2 = new Bike("b2",2);
+        Bike b3 = new Bike("b3",3);
+        bikes[0] = b1;
+        bikes[1] = b2;
+        bikes[2] = b3;
         return bikes;
     }
 
     public static Person[] getPerson() {
         Person[] person = new Person[3];
-        // your code
-
+        Person a = new Person("a","a");
+        Person b = new Person("b","b");
+        Person c = new Person("c","c");
+        person[0] = a;
+        person[1] = b;
+        person[2] = c;
+        //person[0] = new Person("a","a");
         return person;
     }
 
     public static Parcel[] getParcels() {
         Parcel[] parcels = new Parcel[10];
-        // your code
+        Location l = new Location(0.0,0.0);
+        parcels[0] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[1] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[2] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[3] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[4] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[5] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[6] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[7] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[8] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        parcels[9] = new Parcel(1.0,ParcelStatus.SHIPPING,l);
+        
 
         return parcels;
     }
@@ -35,7 +57,7 @@ public class ToyData {
     }
     //testing--------------------------------------------------------------
 
-    private static void printBike() {
+    public static void printBike() {
         Bike[] bikes = getBikes();
         for (int i = 0; i < bikes.length; i++) {
             System.out.println(bikes[i].toString());
